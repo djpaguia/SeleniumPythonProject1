7195709a79460b4a1285ec141aebb68c641f7e0a
djpaguia <dj_paguia@yahoo.com>
2020-12-31 00:11:35 +0800

    made changes to constant.py html testrunner output path added testresults

--- SampleProjectPOM/Utilities/constant.py
+++ SampleProjectPOM/Utilities/constant.py
@@ -4,4 +4,4 @@ class Constant():
     password                       = "admin123"
     url                            = "https://opensource-demo.orangehrmlive.com/"
     chrome_driver_executable_path  = "C:/Users/DJ Paguia/PycharmProjects/Selenium\driver/chromedriver.exe"
-    html_testrunner_output_path    = "C:/Users/DJ Paguia/PycharmProjects/Selenium/SampleProjectPOM/Results"
+    html_testrunner_output_path    = "C:/Users/DJ Paguia/PycharmProjects/SeleniumPythonProject1/SampleProjectPOM/Results"
